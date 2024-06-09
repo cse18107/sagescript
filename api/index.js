a1a6ed4204b865502128c1c74da59d9aa9c76dbe
cse18107 <cse18107@cemk.ac.in>
2024-06-09 12:23:18 +0530

    added verification routes and error handlings

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import userRoute from './routes/user.route.js';
+import authRoute from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,6 +16,23 @@ mongoose.connect(
 
 const app = express();
 
+app.use(express.json())
+
 app.listen('3000', () => {
     console.log('Server is running on port 3000!');
-})
+});
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
