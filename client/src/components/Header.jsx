5bd1bbaeec6c681d0471c9f444713247d00fd267
cse18107 <cse18107@cemk.ac.in>
2024-06-09 21:24:08 +0530

    added header sec

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,52 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+// import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+// import { toggleTheme } from "../redux/theme/themeSlice";
+import { signoutSuccess } from "../redux/user/userSlice";
 import { useEffect, useState } from "react";
-import { AiOutlineSearch } from 'react-icons/ai';
 
 const Header = () => {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  // const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
   return (
     <Navbar className="border-b-2">
       <Link
@@ -16,7 +58,7 @@ const Header = () => {
         </span>
         Script
       </Link>
-      <form onSubmit={()=>console.log('click')}>
+      <form onSubmit={() => console.log("click")}>
         <TextInput
           type="text"
           placeholder="Search..."
@@ -38,7 +80,7 @@ const Header = () => {
         >
           {/* {theme === "light" ? <FaSun /> : <FaMoon />} */}
         </Button>
-        {/* {currentUser ? (
+        {currentUser ? (
           <Dropdown
             arrowIcon={false}
             inline
@@ -58,13 +100,13 @@ const Header = () => {
             <Dropdown.Divider />
             <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
           </Dropdown>
-        ) : ( */}
+        ) : (
           <Link to="/sign-in">
             <Button gradientDuoTone="purpleToBlue" outline>
               Sign In
             </Button>
           </Link>
-        {/* )} */}
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
